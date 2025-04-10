7332a37f964d18e1540a0956c2d3c0f17761ccba
Henriques Maculungungu <henriqueswaldemar1@gmail.com>
2025-04-10 14:39:42 +0100

    Help Message added

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import telebot
 
 # You can set parse_mode by default. HTML or MARKDOWN
-bot = telebot.TeleBot("8138845345:AAF8-3fr5tqnG8tQlp3pfBCnRcRuAmKnmtM",parse_mode=None)
+bot = telebot.TeleBot(
+    "8138845345:AAF8-3fr5tqnG8tQlp3pfBCnRcRuAmKnmtM", parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Olá, Tudo bem ?")
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Como posso ajudalo-lo ?")
 
 
 @bot.message_handler(func=lambda m: True)
